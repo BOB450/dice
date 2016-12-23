b0f6b1953a242d24ecb850b8d773f674239feb17
BOB450 <bwvoskuil@gmail.com>
2016-12-23 10:19:45 -0700

    .exe
    Main

---
+++ Dice roll/Dice roll/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dice_roll
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+          
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            int num = ran.Next(1, 7);
+            label1.Text = num.ToString();
+
+           // if (num == 6)
+           // {
+             //   Application.Exit();
+            //}
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random ran = new Random();
+            int num = ran.Next(1, 100);
+            label1.Text = num.ToString();
+
+        }
+    }
+}
